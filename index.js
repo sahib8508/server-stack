c783848ae4dad31a1aaa9309241ce5718de3a0cc
SAHIB HUSSAIN <sahibhussaib8508@gmail.com>
2023-01-22 11:54:23 +0530

    first commit

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ app.use('/answer', answerRoutes)
 const PORT = process.env.PORT || 5000
 
 const DATABASE_URL = process.env.CONNECTION_URL
-mongoose.connect( DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
-    .catch((err) => console.log(err.message))
+app.listen(process.env.PORT, () => {
+    console.log("Server running...");
+  });
+  
+  /* ----------------------------MONGODB SETUP------------------- */
+  import mongoose from "mongoose";
+  mongoose.set("strictQuery", true);
+  
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB database connected");
+    })
+    .catch((e) => {
+      console.log("db not connected");
+    });
